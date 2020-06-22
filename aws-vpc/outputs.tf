67f8c729c79df00ff131b6ea511a2907a93645be
root <root@ip-10-0-1-98.ec2.internal>
2020-06-22 00:27:11 +0000

    terraform files

---
+++ aws-vpc/outputs.tf
@@ -0,0 +1,14 @@
+output "vpc" {
+  value = "${module.vpc.vpc_id}"
+}
+
+output "subnets" {
+  value = "${module.vpc.subnet_ids}"
+}
+
+output "private_subnets" {
+  value = "${module.vpc.private_subnet_ids}"
+}
+output "security_group" {
+  value = "${aws_security_group.kubernetes.id}"
+}
